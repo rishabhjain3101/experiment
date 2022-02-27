bdbdfdac964ba3604ff09051052828a53f05246a
rishabhjain3101 <rishabh.jain01@quantiphi.com>
2022-02-27 11:03:08 +0530

    Model updated code for commit

--- Model.py
+++ Model.py
@@ -1,2 +1,12 @@
 print("new files")
 
+
+def model1():
+    print("model1 is ready for deployment")
+
+def model2():
+    print("model2 is ready for deployment")
+
+def model3():
+    print("model3 is ready for deployment")
+
